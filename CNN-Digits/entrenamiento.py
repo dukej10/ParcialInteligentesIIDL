e3a1123af0472c4ff945dd0208c72d29e6c4e079
JDuque <diegoduqqe@gmail.com>
2023-05-26 20:23:02 -0500

    cambio cantidad datos prueba y entrenamiento

--- CNN-Digits/entrenamiento.py
+++ CNN-Digits/entrenamiento.py
@@ -37,8 +37,8 @@ numeroCanales=1  # Para trabajar con escala de grises debe ser 1
 formaImagen=(ancho,alto,numeroCanales)
 numeroCategorias=7  # son 10 por se clasificaran en 10 categorias según el numero
 
-cantidaDatosEntrenamiento=[209,209,209,209,209,209,109] # Cuantas imagenes hay para el entrenamiento por cada clase
-cantidaDatosPruebas=[209,209,209,209,209,209,109] # Cuantas imagenes hay para la prueba por cada clase
+cantidaDatosEntrenamiento=[209,209,209,209,209,209,209] # Cuantas imagenes hay para el entrenamiento por cada clase
+cantidaDatosPruebas=[209,209,209,209,209,209,209] # Cuantas imagenes hay para la prueba por cada clase
 
 #Cargar las imágenes
 imagenes, probabilidades=cargarDatos("dataset/train/grays/",numeroCategorias,cantidaDatosEntrenamiento,ancho,alto)
