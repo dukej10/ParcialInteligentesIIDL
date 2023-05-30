f4b0c3ec9431245e87d19598b3b7fe9677e2ce80
JDuque <diegoduqqe@gmail.com>
2023-05-29 19:11:19 -0500

    RECORTE

--- CNN-Digits/editImage.py
+++ CNN-Digits/editImage.py
@@ -2,6 +2,51 @@ import os
 import shutil
 
 import cv2
+import numpy as np
+
+
+def recorte(ruta):
+    # Obtener la ruta del archivo de script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construir la ruta completa de la imagen
+    for n in range(6, 13):
+        for i in range(0, 42):
+            image_path = os.path.join(script_dir, "dataset", "test", str(n), str(n) + '_' + str(i) + '.jpg')
+            # Cargar la imagen
+            img = cv2.imread(image_path)
+
+            # Aplicar un umbral para obtener una imagen binaria
+            _, threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+            # Encontrar contornos en la imagen binaria
+            contours, _ = cv2.findContours(threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            # Iterar sobre cada contorno y crear una imagen para cada uno
+            for i, contour in enumerate(contours):
+                # Crear una imagen en blanco del mismo tamaño que la imagen original
+                contour_image = np.zeros_like(img)
+
+                # Dibujar el contorno en la imagen en blanco
+                cv2.drawContours(contour_image, [contour], -1, (255, 255, 255), cv2.FILLED)
+
+                # Recortar la región dentro del contorno en la imagen original
+                x, y, w, h = cv2.boundingRect(contour)
+                cropped = img[y:y + h, x:x + w]
+
+            # Guardar en test
+            output_folder = os.path.join(script_dir, "dataset", "test", "grays", str(n))
+            os.makedirs(output_folder, exist_ok=True)  # Crear la carpeta si no existe
+            output_path = os.path.join(script_dir, "dataset", "test", "grays", str(n), str(n) + '_' + str(i) + '.jpg')
+            cv2.imwrite(output_path, gray)
+            # Guardar en train
+            output_folder = os.path.join(script_dir, "dataset", "train", "grays", str(n))
+            os.makedirs(output_folder, exist_ok=True)  # Crear la carpeta si no existe
+            output_path = os.path.join(script_dir, "dataset", "train", "grays", str(n), str(n) + '_' + str(i) + '.jpg')
+            cv2.imwrite(output_path, gray)
+
+
+
 
 # Métodos para editar el dataset
 
